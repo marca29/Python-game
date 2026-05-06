2015f2ea97ad362bafa118ad9563b0e8a93062ed
marca29 <marca7298@gmail.com>
2026-05-06 15:27:45 +0200

    Setuped FastAPI, created routes module, added models

---
+++ backend/models.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+class Player(BaseModel):
+    name: str
+
+class Move(BaseModel):
+    match_id: int
+    position: int
+
+class Match(BaseModel):
+    id: int
+    board: List[str]
+    current_player: str
+    players: List[str]
+    winner: Optional[str] = None
+    is_draw: bool = False
